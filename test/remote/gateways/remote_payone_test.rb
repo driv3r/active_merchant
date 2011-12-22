c89062df0081f2454a0f844bc3db20c7a7fc5230
Leszek Zalewski <zalewski_leszek@o2.pl>
2011-12-22 13:35:46 +0100

    generated and improved basic tests for payone gateway

---
+++ test/remote/gateways/remote_payone_test.rb
@@ -0,0 +1,59 @@
+require 'test_helper'
+
+class RemotePayoneTest < Test::Unit::TestCase
+
+
+  def setup
+    @gateway = PayoneGateway.new(fixtures(:payone))
+
+    @amount = 100
+    @credit_card = credit_card('4000100011112224')
+    @declined_card = credit_card('4000300011112220')
+
+    @options = {
+      :order_id => '1',
+      :billing_address => address,
+      :description => 'Store Purchase'
+    }
+  end
+
+  def test_successful_purchase
+    assert response = @gateway.purchase(@amount, @credit_card, @options)
+    assert_success response
+    assert_equal 'This transaction has been approved', response.message
+  end
+
+  def test_unsuccessful_purchase
+    assert response = @gateway.purchase(@amount, @declined_card, @options)
+    assert_failure response
+    assert_equal 'This transaction has not been approved', response.message
+  end
+
+  def test_authorize_and_capture
+    amount = @amount
+    assert auth = @gateway.authorize(amount, @credit_card, @options)
+    assert_success auth
+    assert_equal 'Success', auth.message
+    assert auth.authorization
+    assert capture = @gateway.capture(amount, auth.authorization)
+    assert_success capture
+  end
+
+  def test_failed_capture
+    assert response = @gateway.capture(@amount, '')
+    assert_failure response
+    assert_equal 'This transaction has not been approved', response.message
+  end
+
+  def test_invalid_login
+    gateway = PayoneGateway.new(
+                :login => '',
+                :password => '',
+                :aid => '',
+                :key => ''
+              )
+    assert response = gateway.purchase(@amount, @credit_card, @options)
+    assert_failure response
+    assert_equal 'This transaction has not been approved', response.message
+  end
+end
